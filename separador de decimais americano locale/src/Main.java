6c67d136447bf034815163aca850f3c6aecf7b0d
André Silveira <114875096+Dedecow@users.noreply.github.com>
2022-10-29 17:29:22 -0300

    Add files via upload
    Importando o comando Locale para ter um padrão americano no uso dos números reais.

---
+++ separador de decimais americano locale/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+// esse comando importa o scanner para ser usado no programa
+
+public class Main {
+    public static void main(String[] args) {
+// essa é a declaração do objeto
+        Locale.setDefault(Locale.US);
+        // o formato padrão de numero real agora é USA
+
+        Scanner sc = new Scanner(System.in);
+
+        // aqui está sendo criada a variável que entrará pelo teclado, vai ser encerrada quando não precisar mais dela.
+
+        int lerInteiro;
+        // declarando as variaveis com o tipo na frente, no caso tipo double, ou seja, numeros reais ou com ponto flutuante.
+        Double lerReal;
+
+        System.out.print("Digite um número inteiro: ");
+        lerInteiro = sc.nextInt();
+        // atenção ao tipo de next que deve estar casado com o tipo de variável
+
+        System.out.println(" Você Digitou o número = " + lerInteiro);
+
+        System.out.print("Agora digite um número real: ");
+        lerReal = sc.nextDouble();
+        System.out.printf(" Agora você Digitou o número Real = %.2f%n", lerReal);
+        // o uso do separador entre a parte inteira e decimal do número real se dá de acordo com o teclado, porém a saída é no padráo dos EUA
+
+
+        sc.close();
+        // aqui termina o uso do teclado, o recurso scanner sendo encerrrado
+    }
+}
